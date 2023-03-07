7c9118252c4dab9edd72f81457d14735f4eb0356
Karthick-Prabhakaran <127049119+Karthick-Prabhakaran@users.noreply.github.com>
2023-03-07 15:46:38 +0530

    Delete add_router.py

--- app/add_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-user = APIRouter(prefix='/add',
-                 tags=['addition'])
-
-@user.get('/numbers')
-def adding_numbers():
-    return {"message":"add two numbers"}
